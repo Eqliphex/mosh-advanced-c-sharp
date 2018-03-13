82b92179bd498e14a7521197264776db860ee9d8
Patrick Meyer <tbt_paddik@hotmail.com>
2018-03-13 13:38:23 +0100

    - Completed "Delegates"

--- Delegates/PhotoProcessor.cs
+++ Delegates/PhotoProcessor.cs
@@ -1,11 +1,20 @@
-﻿namespace Delegates
+﻿using System;
+
+namespace Delegates
 {
     public class PhotoProcessor
     {
-        public delegate void PhotoFilterHandler(Photo photo); // Responsible for calling the method/methods with this signature.
+        //public delegate void PhotoFilterHandler(Photo photo); // Responsible for calling the method/methods with this signature. Custom delegate
         
-        public void Process(string path, PhotoFilterHandler filterHandler)
+        
+        // public void Process(string path, PhotoFilterHandler filterHandler)
+        public void Process(string path, Action<Photo> filterHandler)
         {
+            // Difference between Action and Func, is that Func points to methods that return a value, 
+            // whereas Action points at methods who returns void..
+
+            //System.Action<>
+            //System.Func<>
             var photo = Photo.Load(path);
 
             filterHandler(photo);
--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,8 +1,20 @@
 ﻿/*
- * This is an example on how to implement delgates in your program, to the effect 
+ * WHAT:
+ * Is a pointer to a function, or to be more precise, an object that knows
+ * how to call a method or multiple methods.
+ * 
+ * WHY:
+ * To the effect 
  * of having the possibility to extend the framework without changing the underlying
  * programs.
+ * 
+ * WHEN:
+ * When eventing design pattern is used.
+ * or if the caller doesn't need to access other properties or methods in the object 
+ * implementing the method 
  */
+
+using System;
 
 namespace Delegates
 {
@@ -14,13 +26,15 @@ namespace Delegates
             var filters = new PhotoFilters();
 
             // Adds methods to the delegate, note that they all must have the same signature as defined in the delegate.
-            PhotoProcessor.PhotoFilterHandler filterHandler = filters.ApplyBrightness;
+            // PhotoProcessor.PhotoFilterHandler filterHandler = filters.ApplyBrightness; // With custom delegate
+            Action<Photo> filterHandler = filters.ApplyBrightness;
             filterHandler += filters.ApplyContrast;
-            filterHandler += RemoveRedEyeFilter;
+            filterHandler += RemoveRedEyeFilter; 
 
             processor.Process("photo.jpg", filterHandler);
         }
 
+        // A new method to show that the underlying code, isn't changed but that we can just add more to filterhandler.
         static void RemoveRedEyeFilter(Photo photo) // Must match delegate signature
         {
             System.Console.WriteLine("Apply RemoveRedEye");
